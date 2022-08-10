7f14fe9540c275c24913c5d999c4d4e4e1da725f
CarlG96 <carl.gardiner1@hotmail.com>
2022-08-10 15:41:09 +0000

    commit

--- test_student.py
+++ test_student.py
@@ -4,17 +4,33 @@ from student import Student
 
 class TestStudent(unittest.TestCase):
 
-    def test_full_name(self):
-        student = Student('John', 'Smith')
-
-        self.assertEqual(student.full_name, 'John Smith')
-        
+    @classmethod
+    def setUpClass(cls):
+        print('setUpClass')
     
-    def test_alert_santa(self):
-        student = Student('John', 'Smith')
-        student.alert_santa()
-        self.assertTrue(student.naughty_list)
+    @classmethod
+    def tearDownClass(cls):
+        print('tearDownClass')
 
+    def setUp(self):
+        print('setUp')
+        self.student = Student('John', 'Smith')
+
+    def test_full_name(self):
+        print('test full name')
+        self.assertEqual(self.student.full_name, 'John Smith')
+
+    def tearDown(self):
+        print('tearDown')
+        
+    def test_alert_santa(self):
+        print('test alert santa')
+        self.student.alert_santa()
+        self.assertTrue(self.student.naughty_list)
+
+    def test_email(self):
+        print('test email')
+        self.assertTrue(self.student.email, 'john.smith@email.com')
 
 if __name__ == '__main__':
     unittest.main()
